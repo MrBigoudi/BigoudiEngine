06a94e45c01a447b7b408b3929eef0ec0c82d29c
MrBigoudi <yannis.kedadry2@gmail.com>
2023-12-04 22:59:42 +0100

    init vulkan cmake

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
-#include <beCore.hpp>
-#include <beMaths.hpp>
-#include <bePhysics.hpp>
-#include <beRenderer.hpp>
+#include <BigoudiEngine.hpp>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]){
     exit(EXIT_SUCCESS);
---
+++ src/BigoudiEngine.cpp
@@ -0,0 +1,6 @@
+#include "BigoudiEngine.hpp"
+
+#include <beCore.hpp>
+#include <beMaths.hpp>
+#include <bePhysics.hpp>
+#include <beRenderer.hpp>
