306a00f75da3ec3e4f93c335bdf6bd8e0f48df8a
MrBigoudi <yannis.kedadry2@gmail.com>
2024-03-13 12:31:34 +0100

    init light-tree

--- src/beCore/gameplay/be_lights.cpp
+++ src/beCore/gameplay/be_lights.cpp
@@ -60,7 +60,8 @@ void LightUboContainer::init(uint32_t size, VulkanAppPtr vulkanApp){
 }
 
 void LightUboContainer::reset(){
-    _UboData={};
+    _UboData._NbPointLights = 0;
+    _UboData._NbDirectionalLights = 0;
 }
 
 void LightUboContainer::addPointLight(const PointLight& pointLight){
@@ -92,7 +93,7 @@ void LightUboContainer::addDirectionalLight(const DirectionalLight& directionalL
 std::vector<PointLight> LightUboContainer::getPointLights() const{
     std::vector<PointLight> lights{_UboData._NbPointLights};
     for(size_t i=0; i<_UboData._NbPointLights; i++){
-        lights.push_back(_UboData._PointLights[i]);
+        lights[i] = _UboData._PointLights[i];
     }
     return lights;
 }
@@ -100,9 +101,61 @@ std::vector<PointLight> LightUboContainer::getPointLights() const{
 std::vector<DirectionalLight> LightUboContainer::getDirectionalLights() const{
     std::vector<DirectionalLight> lights{_UboData._NbDirectionalLights};
     for(size_t i=0; i<_UboData._NbDirectionalLights; i++){
-        lights.push_back(_UboData._DirectionalLights[i]);
+        lights[i] = _UboData._DirectionalLights[i];
     }
     return lights;
 }
+
+
+LightCutsTree::LightCutsTree(
+        const std::vector<PointLightPtr>& pointLights,
+        const std::vector<DirectionalLightPtr>& directionalLights,
+        const std::vector<OrientedLightPtr>& orientedLights
+    ){
+
+    std::vector<LightCutsTree::LightNodePtr> allNodes = {};
+    createLeaves(pointLights, allNodes);
+    createLeaves(directionalLights, allNodes);
+    createLeaves(orientedLights, allNodes);
+
+    if(allNodes.size() == 0){
+        ErrorHandler::handle(
+            __FILE__, __LINE__,
+            ErrorCode::NOT_INITIALIZED_ERROR,
+            "Can't create a light-tree without lights!\n"
+        );
+    }
+
+    while(allNodes.size() != 1){
+        LightCutsTree::LightNode::mergeTwoBestNodes(allNodes);
+    }
+    _LightsTree = allNodes[0];
+    fprintf(stdout, "tree:\n%s\n", _LightsTree->toString().c_str());
+}
+
+void LightCutsTree::createLeaves(const std::vector<PointLightPtr>& inputLights, std::vector<LightNodePtr>& allNodes) const {
+    for(auto& light : inputLights){
+        LightCutsTree::LightNodePtr newNode = LightCutsTree::LightNode::createNode(light);
+        newNode->_Type = POINT_LIGHT;
+        allNodes.push_back(newNode);
+    }
+}
+
+void LightCutsTree::createLeaves(const std::vector<DirectionalLightPtr>& inputLights, std::vector<LightNodePtr>& allNodes) const {
+    for(auto& light : inputLights){
+        LightCutsTree::LightNodePtr newNode = LightCutsTree::LightNode::createNode(light);
+        newNode->_Type = DIRECTIONAL_LIGHT;
+        allNodes.push_back(newNode);
+    }
+}
+
+void LightCutsTree::createLeaves(const std::vector<OrientedLightPtr>& inputLights, std::vector<LightNodePtr>& allNodes) const {
+    for(auto& light : inputLights){
+        LightCutsTree::LightNodePtr newNode = LightCutsTree::LightNode::createNode(light);
+        newNode->_Type = ORIENTED_LIGHT;
+        allNodes.push_back(newNode);
+    }
+}
+
 
 }
